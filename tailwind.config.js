c77cfffaf385b911a2e8c0caaec760316ac666b3
Ikhwan Alfi <ikhwanalfi228@gmail.com>
2024-12-07 02:35:59 +0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Mencakup semua file HTML dan TypeScript di folder src
+  ],
+  theme: {
+    extend: {}, // Tambahkan kustomisasi tema di sini jika perlu
+  },
+  plugins: [], // Tambahkan plugin Tailwind jika diperlukan
+};
